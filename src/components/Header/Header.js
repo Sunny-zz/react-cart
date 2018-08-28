15dd533a057854e1f6566acc02c7e71a4225e758
Sunny-zz <b695340402@163.com>
2018-08-28 09:35:53 +0800

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 class Header extends Component {
   render() {
     return (
-      <>
+      <div>
         <h1>React shopping-cart</h1>
         <span>2112</span>
-      </>
+      </div>
     )
   }
 }
